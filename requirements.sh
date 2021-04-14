2ee591834245cb85e2367869c5e5cdc4ef53c27c
felipe-mcunha <70486745+felipe-mcunha@users.noreply.github.com>
2021-04-13 21:12:42 -0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,15 +1,18 @@
 echo "Welcome to Helix Sandbox with Security Architeture"
 
 echo "Let's start upggrading Ubuntu"
+sleep 6
 apt update && apt upgrade -y 
 echo "Ubuntu is updated"
-sleep 2
+sleep 6
+
 echo "Now we need Docker and Docker compose, so let's start"
 apt install docker -y && apt install docker-compose -y 
 echo "We have Docker :)"
-sleep 2
+sleep 6
+
 echo "Installing node, wilma needs this"
-sleep 2
+sleep 6
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash - 
 apt-get install -y nodejs 
 apt-get install gcc g++ make -y 
@@ -17,14 +20,14 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 
 apt-get update && sudo apt-get install yarn -y 
 echo "I can see that we have node, wilma is happy"
-sleep 2
+sleep 6
 
 echo "The gran finale, MYSQL, keyrock need this guy, (They are best friends)"
-sleep 2
+sleep 6
 apt install mysql-sandbox -y
 echo "Well, we have everything, so let's go to work, and remember have some fun"
 echo "Byyyyyye :)"
-sleep 1
+sleep 16
 
 clear
 
